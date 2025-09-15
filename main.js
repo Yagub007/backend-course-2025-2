ea1c20467ddf7e79fae6da8b4426ce03737ac538
Ягуб bc2025-2 <aliev092006@gmail.com>
2025-09-15 14:49:13 +0300

    Fixed main.js for correct URL

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 const { URL, URLSearchParams } = require("url");
 
-const date = "202408";       
-const time_period = "m";    
+const date = "202408";
+const time_period = "m";
 
-const base = "https://bank.gov.ua/NBUStatService/v1/statdirectory/bank_incomeexp";
+const base = "https://bank.gov.ua/NBUStatService/v1/statdirectory/banksincomeexp";
 
 const url = new URL(base);
 
-url.search = new URLSearchParams({
-  date: date,
-  period: time_period,
-  json: ""
-});
+const params = new URLSearchParams();
+params.set("date", date);
+params.set("period", time_period);
+params.set("json", ""); 
+
+const search = params.toString().replace(/json=$/, "json");
+
+url.search = search;
 
 console.log(url.toString());
+
